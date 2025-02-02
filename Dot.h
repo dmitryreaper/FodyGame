07367a64a363d4ce5c6efaec44a1cb9e7cfc71ef
dmitryreaper <dimadimof81@gmail.com>
2025-02-02 12:58:25 +0000

    update: header dot.h

--- Dot.h
+++ Dot.h
@@ -5,14 +5,27 @@
 
 class Dot {
 public:
+    // Конструктор
     Dot(int x, int y);
 
+    // Обработка событий клавиатуры
     void handleEvent(SDL_Event& e, bool* keys);
+
+    // Движение точки
     void move(bool* keys, Uint32 deltaTime, int screenWidth, int screenHeight);
+
+    // Отрисовка точки
     void render(SDL_Renderer* renderer);
 
+    // Геттеры для позиции
+    float getX() const { return mPosX; }
+    float getY() const { return mPosY; }
+	
+	float mPosX, mPosY; // позиция точки
+    float mVelX, mVelY; // Скорость по осям 
+
 private:
-    float mPosX, mPosY;
+
 };
 
 #endif
